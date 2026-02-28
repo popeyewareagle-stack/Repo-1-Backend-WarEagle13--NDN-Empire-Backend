04c21f5a1572f6915ca11ecef144b0f6acf30633
popeyewareagle-stack <popeyewareagle@gmail.com>
2026-02-27 21:22:59 -0600

    Create servers.js

---
+++ servers.js
@@ -0,0 +1,9 @@
+import app from "./src/app.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("🔥 WarEagle13 NDN Empire Backend LIVE on port " + PORT);
+});
